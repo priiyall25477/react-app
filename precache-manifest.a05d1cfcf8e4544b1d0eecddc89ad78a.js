c03fa6bd952498ea40d633c9dee4d0bd589bc709
priyal daga <priyaldaga98@gmail.com>
2020-09-19 15:49:41 +0530

    Updates

---
+++ precache-manifest.a05d1cfcf8e4544b1d0eecddc89ad78a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812962ee8bc4b04fc5c7f6fb08f5d335",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "d0a74893fb1ed6901856",
+    "url": "/react-app/static/css/main.8a730d5f.chunk.css"
+  },
+  {
+    "revision": "5b32d6c761615fb20451",
+    "url": "/react-app/static/js/2.d4cdc17e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-app/static/js/2.d4cdc17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a74893fb1ed6901856",
+    "url": "/react-app/static/js/main.a2eaf3bd.chunk.js"
+  },
+  {
+    "revision": "a8dd6d1ad082fe3a3b60",
+    "url": "/react-app/static/js/runtime-main.bc8f1399.js"
+  },
+  {
+    "revision": "f11040fd192dde12039a874220e59677",
+    "url": "/react-app/static/media/2.f11040fd.jpg"
+  },
+  {
+    "revision": "89a2f1d140b337b29ce3c9b5cbf82d12",
+    "url": "/react-app/static/media/3.89a2f1d1.png"
+  },
+  {
+    "revision": "30a7a52afebab17f005cabd008916d09",
+    "url": "/react-app/static/media/4.30a7a52a.png"
+  },
+  {
+    "revision": "780bf5232d8d3c6fde4c1018de5d26d4",
+    "url": "/react-app/static/media/5.780bf523.png"
+  },
+  {
+    "revision": "adfc6d1d4923bd287fb8dd47444a9e2b",
+    "url": "/react-app/static/media/6.adfc6d1d.png"
+  },
+  {
+    "revision": "9d7b0916907d22e3eafc369a9bba48e0",
+    "url": "/react-app/static/media/7.9d7b0916.png"
+  }
+]);
